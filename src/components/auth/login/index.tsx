9dd4b2f8d4168a21e3200f711bbc63f6662e3543
Lohan Martz <lohan.fidelis@gmail.com>
2022-08-12 13:14:50 -0300

    Changes: Responsive login screen

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Col, Form, FormGroup, Input, Label, Button } from 'reactstrap';
+import { Col, Form, FormGroup, Input, Label, Button, Row, Container } from 'reactstrap';
 import Cookies from 'universal-cookie';
 import { useNavigate } from 'react-router-dom';
 import { setAuthLocalStorage, getLocalStorage } from '../../../helpers/localStorage';
@@ -72,80 +72,92 @@ const Login = (): JSX.Element => {
   return (
     <>
       <Loader />
-      <div className="login-card">
-        <div>
-          <div className="login-main login-tab">
-            <a className="logo text-center" href="#javascript">
-              <img className="img-fluid for-light" src={logoBanca} alt="looginpage" />
-            </a>
-            <Form className="loginCard" noValidate={true} onSubmit={handleSubmit}>
-              <div style={{ marginBottom: '50px' }}>
-                <div className="pageTitle">Seja bem-vindo(a)! Entre com a sua conta</div>
-                <div className="subTitleMain">Digite abaixo o seu CPF e sua senha para entrar</div>
-              </div>
-              <div className="form-row ">
-                <Col className="loginField">
-                  <Label className="loginFormLabel">
-                    <img className="mr-2" src={idCard} />
-                    Seu CPF
-                  </Label>
-                  <Input
-                    name="username"
-                    className="form-control loginForm"
-                    required={true}
-                    placeholder="Digite seu CPF"
-                    value={form?.username ? cpfMask(form?.username) : ''}
-                    onChange={handleChange}
-                    isValid={!errors.username}
-                    maxLength={14}
-                  />
-                  <span>{errors.username && 'O campo do CPF é obrigatório!'}</span>
-                  <div className="valid-feedback">{'Looks good!'}</div>
-                </Col>
-                <Col>
-                  <Label className="loginFormLabel">
-                    <img className="mr-2" src={loginLock} />
-                    Senha
-                  </Label>
-                  <Input
-                    className="form-control loginForm"
-                    type={togglePassword ? 'text' : 'password'}
-                    required={true}
-                    placeholder="Digite sua senha"
-                    name="password"
-                    value={form?.password || ''}
-                    onChange={handleChange}
-                  />
-                  <div className="show-hide" onClick={() => HideShowPassword(togglePassword)}>
-                    <img
-                      className="d-flex justify-content-center align-items-center passwordIcon"
-                      src={eye}
-                    />
-                  </div>
-                  <span>{errors.username && 'O campo senha é obrigatório!'}</span>
-                  <div className="valid-feedback">{'Looks good!'}</div>
-                </Col>
-              </div>
-              <FormGroup className="d-flex justify-content-between align-items-center mb-2">
-                <div className="checkbox ml-3" style={{ marginTop: '25px' }}>
-                  <Input id="checkbox1" type="checkbox" onChange={() => setIsCheck(!isCheck)} />
-                  <Label className="loginFormLabel" for="checkbox1">
-                    Mantenha-me conectado
-                  </Label>
+      <Container fluid={true}>
+        <Row lg="2" md="1">
+          <Col xs="auto" className="login-card">
+            <div className="login-main login-tab">
+              <a className="logo text-center" href="#javascript">
+                <img className="img-fluid for-light" src={logoBanca} alt="looginpage" />
+              </a>
+              <Form className="loginCard" noValidate={true} onSubmit={handleSubmit}>
+                <Row>
+                  <Col md="12">
+                    <div style={{ marginBottom: '50px', display: 'contents' }}>
+                      <div className="pageTitle">Seja bem-vindo(a)! Entre com a sua conta</div>
+                      <div className="subTitleMain">
+                        Digite abaixo o seu CPF e sua senha para entrar
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+                <div className="form-row ">
+                  <Row>
+                    <Col className="loginField">
+                      <Label className="loginFormLabel">
+                        <img className="mr-2" src={idCard} />
+                        Seu CPF
+                      </Label>
+                      <Input
+                        name="username"
+                        className="form-control loginForm"
+                        required={true}
+                        placeholder="Digite seu CPF"
+                        value={form?.username ? cpfMask(form?.username) : ''}
+                        onChange={handleChange}
+                        isValid={!errors.username}
+                        maxLength={14}
+                      />
+                      <span>{errors.username && 'O campo do CPF é obrigatório!'}</span>
+                      <div className="valid-feedback">{'Looks good!'}</div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Label className="loginFormLabel">
+                        <img className="mr-2" src={loginLock} />
+                        Senha
+                      </Label>
+                      <Input
+                        className="form-control loginForm"
+                        type={togglePassword ? 'text' : 'password'}
+                        required={true}
+                        placeholder="Digite sua senha"
+                        name="password"
+                        value={form?.password || ''}
+                        onChange={handleChange}
+                      />
+                      <div className="show-hide" onClick={() => HideShowPassword(togglePassword)}>
+                        <img
+                          className="d-flex justify-content-center align-items-center passwordIcon"
+                          src={eye}
+                        />
+                      </div>
+                      <span>{errors.username && 'O campo senha é obrigatório!'}</span>
+                      <div className="valid-feedback">{'Looks good!'}</div>
+                    </Col>
+                  </Row>
                 </div>
-              </FormGroup>
-              <Button className="mainButton" style={{ marginTop: '60px' }}>
-                <div className="loginFormText">Entrar</div>
-              </Button>
-              <div className="d-flex justify-content-center align-items-center forgotPassword">
-                <a onClick={() => history('/forget-pwd')} className="link">
-                  Esqueceu a sua senha?
-                </a>
-              </div>
-            </Form>
-          </div>
-        </div>
-      </div>
+                <FormGroup className="d-flex justify-content-between align-items-center mb-2">
+                  <div className="checkbox ml-3" style={{ marginTop: '25px' }}>
+                    <Input id="checkbox1" type="checkbox" onChange={() => setIsCheck(!isCheck)} />
+                    <Label className="loginFormLabel" for="checkbox1">
+                      Mantenha-me conectado
+                    </Label>
+                  </div>
+                </FormGroup>
+                <Button className="mainButton" style={{ marginTop: '60px' }}>
+                  <div className="loginFormText">Entrar</div>
+                </Button>
+                <div className="d-flex justify-content-center align-items-center forgotPassword">
+                  <a onClick={() => history('/forget-pwd')} className="link">
+                    Esqueceu a sua senha?
+                  </a>
+                </div>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
