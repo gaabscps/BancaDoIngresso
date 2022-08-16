e76738d3cf1883c231d0227ec3b9bbb255967ddf
Lohan Martz <lohan.fidelis@gmail.com>
2022-08-16 15:44:30 -0300

    Validações Yup

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,18 +1,22 @@
-import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Col, Form, FormGroup, Input, Label, Button, Row, Container } from 'reactstrap';
+import React, { useEffect, useState, ChangeEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { Col, Form, Button, Row, Container } from 'reactstrap';
 import Cookies from 'universal-cookie';
 import { useNavigate } from 'react-router-dom';
-import { setAuthLocalStorage, getLocalStorage } from '../../../helpers/localStorage';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Input from '../../Utils/Input';
+import { getLocalStorage } from '../../../helpers/localStorage';
 import Loader from '../../../layout/loader';
 import { cpfMask } from '../../../utils/input-mask';
 import logoBanca from '../../../assets/images/logo/logoBanca.png';
 import idCard from '../../../assets/images/svg/idCard.svg';
 import loginLock from '../../../assets/images/svg/loginLock.svg';
 import eye from '../../../assets/images/login/eye.png';
-import { ApplicationState } from '../../../store';
+// import { ApplicationState } from '../../../store';
 import { loginRequest } from '../../../store/ducks/auth/actions';
-import { AuthState } from '../../../store/ducks/auth/types';
+// import { AuthState } from '../../../store/ducks/auth/types';
 import { setAuthSessionStorage } from '../../../helpers/sessionStorage';
 import Auth from '../../../entities/Auth';
 
@@ -21,30 +25,36 @@ interface Loginstate {
   password: string;
 }
 
+const schema = yup.object().shape({
+  username: yup.string().min(14, 'CPF Inválido').required('O CPF é obrigatório'),
+  password: yup.string().required('A senha é obrigatória'),
+});
+
 const Login = (): JSX.Element => {
-  const auth = useSelector<ApplicationState, AuthState>(store => store.auth);
-  const [isCheck, setIsCheck] = useState(false);
-  if (!auth.loading && !auth.error) {
-    if (auth.data && auth.data.login) {
-      setAuthLocalStorage(auth.data.login, isCheck);
-      window.location.href = '/';
-    }
-  }
+  // const auth = useSelector<ApplicationState, AuthState>(store => store.auth);
+  // if (!auth.loading && !auth.error) {
+  //   if (auth.data && auth.data.login) {
+  //     setAuthLocalStorage(auth.data.login);
+  //     window.location.href = '/';
+  //   }
+  // }
   const history = useNavigate();
   const dispatch = useDispatch();
   const [togglePassword, setTogglePassword] = useState(false);
   // const [values, setValues] = useState({ cpf: "" });
   const [form, setForm] = useState({} as Loginstate);
-  const [errors] = useState({
-    username: false,
-    password: false,
-  });
-
   const cookies = new Cookies();
   const HideShowPassword = (tPassword: boolean): void => {
     setTogglePassword(!tPassword);
   };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
   useEffect(() => {
     if (getLocalStorage('token')) {
       setAuthSessionStorage({
@@ -55,9 +65,10 @@ const Login = (): JSX.Element => {
     }
   }, [cookies, history]);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
-    e.preventDefault();
-    dispatch(loginRequest(form.username, form.password));
+  const onSubmit = async (data: any): Promise<void> => {
+    // e.preventDefault();
+
+    dispatch(loginRequest(data.username, data.password));
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
@@ -79,7 +90,7 @@ const Login = (): JSX.Element => {
               <a className="logo text-center" href="#javascript">
                 <img className="img-fluid for-light" src={logoBanca} alt="looginpage" />
               </a>
-              <Form className="loginCard" noValidate={true} onSubmit={handleSubmit}>
+              <Form className="loginCard" noValidate={true} onSubmit={handleSubmit(onSubmit)}>
                 <Row>
                   <Col md="12">
                     <div style={{ marginBottom: '50px', display: 'contents' }}>
@@ -93,38 +104,52 @@ const Login = (): JSX.Element => {
                 <div className="form-row ">
                   <Row>
                     <Col className="loginField">
-                      <Label className="loginFormLabel">
+                      {/* <Label className="loginFormLabel">
                         <img className="mr-2" src={idCard} />
                         Seu CPF
-                      </Label>
-                      <Input
+                      </Label> */}
+                      {/* <Input
                         name="username"
                         className="form-control loginForm"
                         required={true}
                         placeholder="Digite seu CPF"
                         value={form?.username ? cpfMask(form?.username) : ''}
                         onChange={handleChange}
-                        isValid={!errors.username}
                         maxLength={14}
+                      /> */}
+                      <Input
+                        label={
+                          <p>
+                            <img className="mr-2" src={idCard} />
+                            {'Seu CPF'}
+                          </p>
+                        }
+                        type="text"
+                        placeholder="Digite o seu CPF"
+                        register={register}
+                        value={form?.username ? cpfMask(form?.username) : ''}
+                        name="username"
+                        onChange={handleChange}
+                        error={errors?.username?.message}
                       />
-                      <span>{errors.username && 'O campo do CPF é obrigatório!'}</span>
-                      <div className="valid-feedback">{'Looks good!'}</div>
                     </Col>
                   </Row>
                   <Row>
                     <Col>
-                      <Label className="loginFormLabel">
-                        <img className="mr-2" src={loginLock} />
-                        Senha
-                      </Label>
                       <Input
-                        className="form-control loginForm"
+                        label={
+                          <p>
+                            <img className="mr-2" src={loginLock} />
+                            {'Senha'}
+                          </p>
+                        }
                         type={togglePassword ? 'text' : 'password'}
-                        required={true}
-                        placeholder="Digite sua senha"
+                        placeholder="Digite o seu CPF"
+                        register={register}
+                        value={form?.password}
                         name="password"
-                        value={form?.password || ''}
                         onChange={handleChange}
+                        error={errors?.password?.message}
                       />
                       <div className="show-hide" onClick={() => HideShowPassword(togglePassword)}>
                         <img
@@ -132,19 +157,9 @@ const Login = (): JSX.Element => {
                           src={eye}
                         />
                       </div>
-                      <span>{errors.username && 'O campo senha é obrigatório!'}</span>
-                      <div className="valid-feedback">{'Looks good!'}</div>
                     </Col>
                   </Row>
                 </div>
-                <FormGroup className="d-flex justify-content-between align-items-center mb-2">
-                  <div className="checkbox ml-3" style={{ marginTop: '25px' }}>
-                    <Input id="checkbox1" type="checkbox" onChange={() => setIsCheck(!isCheck)} />
-                    <Label className="loginFormLabel" for="checkbox1">
-                      Mantenha-me conectado
-                    </Label>
-                  </div>
-                </FormGroup>
                 <Button className="mainButton" style={{ marginTop: '60px' }}>
                   <div className="loginFormText">Entrar</div>
                 </Button>
