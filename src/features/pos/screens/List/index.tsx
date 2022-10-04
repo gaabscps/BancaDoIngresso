38ad36765a19cd4761522b0cd511b6e048182955
gaabscps <gaabscps@gmail.com>
2022-10-04 18:22:14 -0300

    💄 Modal exclude & Pos Table
    Ajustado texto de modal de excluir em todas as paginas e empty state da coluna da tabela de POS

--- src/features/pos/screens/List/index.tsx
+++ src/features/pos/screens/List/index.tsx
@@ -244,7 +244,7 @@ export const PosScreen: React.FC = (): JSX.Element => {
           onClick: (): void => handleOnClose(),
         },
         {
-          title: 'Sim, quero remover',
+          title: 'Sim, quero excluir',
           onClick: (): Promise<void> => handleOnConfirmDeleteToPos(posSelected),
         },
       ],
