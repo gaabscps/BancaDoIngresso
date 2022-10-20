73c5778ef5f2a64aa11d85da3c58bf0f5724458a
André Santos <dev.santosandre@gmail.com>
2022-10-20 11:42:59 -0300

    🚀  BDIDEV-275

--- src/features/product/components/RegisterContent/index.tsx
+++ src/features/product/components/RegisterContent/index.tsx
@@ -33,7 +33,7 @@ export const RegisterContent: React.FC<RegisterContentProps> = ({
   formErrors,
   nameFiles,
   listProductGroup,
-  listProductSubGroup,
+  // listProductSubGroup,
   handleFecthProductSubGroupList,
   onChangeFormInput,
   onChangeFileInput,
@@ -79,7 +79,8 @@ export const RegisterContent: React.FC<RegisterContentProps> = ({
               onChange={e => onChangeFormInput(FormInputName.subGroupProduct)(e?.value as string)}
               error={formErrors.subGroupProduct && formErrors.subGroupProduct[0]}
               value={formData[FormInputName.subGroupProduct]}
-              options={listProductSubGroup.map(item => ({ value: item.id, label: item.name }))}
+              options={[]}
+              // options={listProductSubGroup.map(item => ({ value: item.id, label: item.name }))}
             />
           </FormGroup>
           <FormGroup className="mb-2">
