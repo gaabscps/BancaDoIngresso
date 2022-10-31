55767afba193edf6e41d23d73f4fc984abe4127e
gaabscps <gaabscps@gmail.com>
2022-10-31 11:09:19 -0300

    ✨ Componente Exibir Adidionado componente de paginação usado na pagina de eventos

---
+++ src/components/SimpleSelect/index.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React, { useState } from 'react';
+
+// react-select with props typescript types
+import Select, { SingleValue, ActionMeta } from 'react-select';
+import { customStyles } from './style';
+
+interface OptionProps {
+  value?: string | number;
+  label: string;
+  target?: HTMLInputElement;
+}
+
+interface SimpleSelectProps {
+  name: string;
+  label?: string;
+  error?: string;
+  onChange?: (newValue: SingleValue<OptionProps>, actionMeta: ActionMeta<OptionProps>) => void;
+  value: number;
+  options: OptionProps[];
+  placeholder?: string;
+  id?: string;
+  style?: React.CSSProperties;
+  wrapperClass?: string;
+  control?: any;
+  defaultValue?: OptionProps;
+  onBlur?: () => void;
+  ref?: any;
+}
+
+export const SimpleSelect = (props: SimpleSelectProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const onMenuOpen = () => setIsMenuOpen(true);
+  const onMenuClose = () => setIsMenuOpen(false);
+
+  return (
+    <>
+      <div className={`flex-column ${props.wrapperClass}`}>
+        {props.label && (
+          <label htmlFor={props.name} className="pagination-change-label">
+            {props.label}
+          </label>
+        )}
+        <Select
+          options={props.options}
+          placeholder={props.placeholder}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+          styles={customStyles()}
+          noOptionsMessage={() => 'Nenhum resultado encontrado'}
+          value={props.options.find((option: OptionProps) => option.value === props.value)}
+          defaultValue={props.defaultValue}
+          ref={props.ref}
+          isSearchable={false}
+        />
+        {props.error && (
+          <span role="alert" className="alert-error">
+            {props.error}
+          </span>
+        )}
+      </div>
+    </>
+  );
+};
---
+++ src/components/SimpleSelect/style.ts
@@ -0,0 +1,100 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const customStyles = () => ({
+  control: (provided: any, state: any) => ({
+    ...provided,
+    // width: '546px',
+    width: '200px',
+    borderRadius: '5px',
+    backgroundColor: state.isFocused ? 'rgba(12, 2, 1, 0.2)' : 'transparent',
+    border: '2px solid #222222;',
+    boxShadow: state.isFocused && 'none',
+    padding: '2px 0',
+    '&:hover': {
+      backgroundColor: 'rgba(12, 2, 1, 0.08)',
+    },
+    '&:active': {
+      backgroundColor: 'rgba(12, 2, 1, 0.2)',
+    },
+  }),
+
+  valueContainer: (provided: any) => ({
+    ...provided,
+    height: '2.625rem',
+    padding: '1px 12px',
+    fontSize: '16px',
+    fontWeight: '500;',
+  }),
+
+  input: (provided: any) => ({
+    ...provided,
+    margin: '0px',
+  }),
+  indicatorSeparator: () => ({
+    display: 'none',
+  }),
+  indicatorsContainer: (provided: any) => ({
+    ...provided,
+    height: '30px',
+    margin: 'auto 0',
+  }),
+  dropdownIndicator: (provided: any) => ({
+    ...provided,
+    color: '#000',
+    padding: '0px 12px',
+  }),
+  menu: (provided: any, state: any) => ({
+    ...provided,
+    width: '200px',
+    borderRadius: '5px',
+    backgroundColor: 'transparent',
+    boxShadow: state.isFocused && 'none',
+    marginTop: '0px',
+    zIndex: 999,
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      top: '3px',
+      right: '-4px',
+      transform: 'translateX(-50%)',
+      borderLeft: '10px solid transparent',
+      borderRight: '10px solid transparent',
+      borderBottom: '10px solid rgba(1, 2, 1, 0.2)',
+    },
+    '&:hover:before': {
+      borderBottom: '10px solid #E6E6E6',
+    },
+  }),
+
+  menuList: (provided: any) => ({
+    ...provided,
+    padding: '0px',
+    fontSize: '16px',
+    fontWeight: '500;',
+    right: '29px',
+    width: '229px',
+    border: '1px solid #E6E6E6',
+    borderRadius: '10px',
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    marginTop: '10px',
+  }),
+
+  option: (provided: any, state: any) => ({
+    ...provided,
+    backgroundColor: state.isSelected ? '#E6E6E6' : 'transparent',
+    color: '#000',
+    fontSize: '16px',
+    padding: '10px 12px',
+    '&:hover': {
+      backgroundColor: 'rgba(12, 2, 1, 0.08)',
+    },
+    '&:active': {
+      backgroundColor: 'rgba(12, 2, 1, 0.2)',
+    },
+  }),
+
+  placeholder: (provided: any) => ({
+    ...provided,
+    color: '#000',
+    fontSize: '16px',
+  }),
+});
