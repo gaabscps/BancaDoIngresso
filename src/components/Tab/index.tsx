09b5b8045915a683e14defb8a36775165ea1b047
André Santos <dev.santosandre@gmail.com>
2022-11-10 17:48:21 -0300

    🚧 BDIDEV-432
    Feature tabs

--- src/components/Tab/index.tsx
+++ src/components/Tab/index.tsx
@@ -13,21 +13,28 @@ export const Tab = ({ titles, contents }) => {
   return (
     <div className="tab-component">
       <div className="container-header">
-        <ul className="tab-list">
+        <div className="tab-list">
           {titles.map((title, index) => (
-            <li
+            <div
               key={index}
               className={`tabs ${getActiveClass(index, 'active-tabs')}`}
               onClick={() => toggleTab(index)}
             >
               {title}
-            </li>
+              <div className={`${getActiveClass(index, 'active-indicator')}`} />
+            </div>
           ))}
-        </ul>
+        </div>
         <div className="content-container">
           {contents.map((content, index) => (
-            <div key={index} className={`content ${getActiveClass(index, 'active-content')}`}>
-              <h2>{content}</h2>
+            <div
+              key={index}
+              className={`content mainContainer container-fluid ${getActiveClass(
+                index,
+                'active-content',
+              )}`}
+            >
+              {content}
             </div>
           ))}
         </div>
