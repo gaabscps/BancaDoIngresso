74a625f2f42db2af2ff8c542af6949b1979f24ee
gaabscps <gaabscps@gmail.com>
2022-12-03 01:10:40 -0300

    :beers: BDIDEV-625
    Necessário ajuste na arquitetura

--- src/features/registerEvent/components/PdvUserScreen/types/index.ts
+++ src/features/registerEvent/components/PdvUserScreen/types/index.ts
@@ -1,8 +1,15 @@
 import { FormData, FormErrors, OnChangeFormInput, IsFormValid } from '@/hooks';
+import { ShouldShowModal } from '../screens/ui';
 
 export interface formPdvUserProps {
   formData: FormData;
   formErrors: FormErrors;
   onChangeFormInput: OnChangeFormInput;
   isFormValid: IsFormValid;
+}
+
+export interface onShouldShowModalSectorProductUserProps {
+  value: ShouldShowModal;
+  newTitleModal: string | React.ReactNode;
+  user?: any;
 }
