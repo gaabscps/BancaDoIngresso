2c2b52face88741ac5fafb9acab7e0e64e5b627b
André Santos <dev.santosandre@gmail.com>
2022-12-16 12:31:30 -0300

    💩 BDIDEV-000
    Revertendo alterações

--- src/features/registerEvent/screens/index.tsx
+++ src/features/registerEvent/screens/index.tsx
@@ -29,10 +29,6 @@ export const EventScreen: React.FC = (): JSX.Element => {
 
   const steps = [
     {
-      Component: <SectorProductScreen />,
-      title: 'Setor e Produto',
-    },
-    {
       Component: <GeneralInformationScreen />,
       title: 'Informações gerais',
     },
@@ -40,10 +36,10 @@ export const EventScreen: React.FC = (): JSX.Element => {
       Component: <SectorTicketScreen />,
       title: 'Setor e Ingresso',
     },
-    // {
-    //   Component: <SectorProductScreen />,
-    //   title: 'Setor e Produto',
-    // },
+    {
+      Component: <SectorProductScreen />,
+      title: 'Setor e Produto',
+    },
     {
       Component: <PdvEventScreen />,
       title: 'PDV',
